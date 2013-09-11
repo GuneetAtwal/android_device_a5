1169c154ddb5587906c90035d93209b76ff280bc
GuneetAtwal <harjinder767@gmail.com>
2013-09-11 23:29:43 +0530

    more blobs,elements and CAF Properties added

--- a5/a5.mk
+++ a5/a5.mk
@@ -1,38 +1,34 @@
-#
-# Copyright 2012 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+include device/karbonn/a5/qcom/msm7627a.mk
 
-# Sample: This is where we'd set a backup provider if we had one
-# $(call inherit-product, device/sample/products/backup_overlay.mk)
+DEVICE_PACKAGE_OVERLAYS := device/karbonn/a5/overlay
 
-# Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
-
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/karbonn/a5/a5.mk)
-
-## Device identifier. This must come after all inclusions
+PRODUCT_NAME := a5
 PRODUCT_DEVICE := a5
-PRODUCT_NAME := cm_a5
-PRODUCT_BRAND := Karbonn
-PRODUCT_MODEL := a5
-PRODUCT_MANUFACTURER := Karbonn
-PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_PACKAGES += \
+    sensors.msm7627a
+
+PRODUCT_PACKAGES += \
+    SystemUI_Ext \
+    Apollo \
+    CMFileManager \
+    superuser \
+    DSPManager \
+    libcyanogen-dsp \
+     audio_effects.conf \
+     su
+
+# Bluetooth configuration files
+PRODUCT_COPY_FILES += \
+    device/karbonn/a5/prebuilt/fts_touch.idc:system/usr/idc/fts_touch.idc \
+    device/karbonn/a5/prebuilt/ms-msg21xx.idc:system/usr/idc/ms-msg21xx.idc \
+    device/karbonn/a5/prebuilt/fts_touch.kl:system/usr/keylayout/fts_touch.kl \
+    device/karbonn/a5/prebuilt/ms-msg21xx.kl:system/usr/keylayout/ms-msg21xx.kl
+
+PRODUCT_PACKAGES += vold.fstab
+
+PRODUCT_COPY_FILES += device/karbonn/a5/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.product.model= Karbonn_A5 \
+  ro.sf.lcd_density=160
